e31eca86bc0a119dede9dfa34e1e6ad69d226eda
bo.wei <bo.wei@ifchange.com>
2019-06-22 14:15:10 +0800

    初始化文件

---
+++ precache-manifest.0b1c9533ce75c6ed19546e5c5a855f25.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713f226a123059f3df96277b7cfc28d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7ca5d15e817f7beb23",
+    "url": "/static/css/main.e929528f.chunk.css"
+  },
+  {
+    "revision": "6943d1a07aa2723143f2",
+    "url": "/static/js/2.ea10df0f.chunk.js"
+  },
+  {
+    "revision": "ac7ca5d15e817f7beb23",
+    "url": "/static/js/main.48793594.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b34126f34bad587bef64ea1b3ce18c45",
+    "url": "/static/media/home-bg.b34126f3.png"
+  },
+  {
+    "revision": "b791dbc011659107411fb5a51e7a2aa5",
+    "url": "/static/media/logo.b791dbc0.png"
+  },
+  {
+    "revision": "039eb439741141dc6581abf150da0d7b",
+    "url": "/static/media/qrcode.039eb439.png"
+  },
+  {
+    "revision": "f51b9c0cc81f1c8b8d2b231023680fbb",
+    "url": "/static/media/sec-1-2.f51b9c0c.png"
+  },
+  {
+    "revision": "7d390c2425c57057d98b88139539f2f2",
+    "url": "/static/media/sec-2-3.7d390c24.png"
+  },
+  {
+    "revision": "4cfab86869999878a4a861826aeeb7cb",
+    "url": "/static/media/section-2.4cfab868.png"
+  },
+  {
+    "revision": "603dba0f2e624818c6510b26048a929a",
+    "url": "/static/media/section-3.603dba0f.png"
+  },
+  {
+    "revision": "d2e1e9a8c7ffcfcd0675edc991b5f65f",
+    "url": "/static/media/section-4.d2e1e9a8.png"
+  },
+  {
+    "revision": "bc27cac1fbc8449d40a0d90282da9b5f",
+    "url": "/static/media/section-6.bc27cac1.png"
+  },
+  {
+    "revision": "3d4127bcefd8e70b3f31a16f347a091f",
+    "url": "/static/media/section-7.3d4127bc.png"
+  },
+  {
+    "revision": "5fc3b0a5dbee147ed718094bd36d6be1",
+    "url": "/static/media/shangwuhezuo.5fc3b0a5.png"
+  }
+]);
