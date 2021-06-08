d28b526582f46dd9f2e823892ab9813042d5906d
bo.wei <bo.wei@mobilemd.cn>
2021-06-08 13:58:00 +0800

    chore

---
+++ packages/eslint/tsx/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      browser: true,
+      node: true,
+      es2021: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+      'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+    ],
+    rules: {},
+  };
+  
