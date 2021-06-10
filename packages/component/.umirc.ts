604af2bab57923a5cd09061444c7efb5947b782a
bo.wei <bo.wei@mobilemd.cn>
2021-06-10 11:26:32 +0800

    整理代码

--- packages/component/.umirc.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-    title: '@tms/site-component',
-    description: 'react site componet',
-    outputPath: "docs_dist",
-});
--- packages/hook/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/monorepo/' : '/',
-    title: '@tms/site-hook',
-    description: 'hooks',
-    outputPath: "docs_dist",
-    base: '/monorepo/'
-});
