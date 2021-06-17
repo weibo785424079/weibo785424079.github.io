b1fabd8ae2791ff42865eb25ff01b6dc0d596f1f
bo.wei <bo.wei@mobilemd.cn>
2021-06-17 14:22:47 +0800

    再次修改eslint

--- packages/eslint/ts/index.js
+++ packages/eslint/ts/index.js
@@ -19,8 +19,6 @@ module.exports = {
   rules: {
     'no-use-before-define': 0,
     'import/no-extraneous-dependencies': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'] }],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
     'import/extensions': ['error', 'never', { svg: 'always' }],
