dc220e21d82750ecf5ab14490fbce91da1696fe8
bo.wei <bo.wei@mobilemd.cn>
2021-07-08 16:13:49 +0800

    feat(add prettier): add prettier
    affects: @tms/eslint-config-site

--- packages/eslint/ts/index.js
+++ packages/eslint/ts/index.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
     node: true,
+    es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 0,
     'import/no-extraneous-dependencies': 0,
--- packages/eslint/tsx/index.js
+++ packages/eslint/tsx/index.js
@@ -5,6 +5,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'plugin:prettier/recommended',
     'plugin:react/recommended',
     'airbnb',
     'plugin:import/typescript',
@@ -17,15 +18,15 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['prettier', 'react', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 0,
     'import/no-extraneous-dependencies': 0,
     'react/jsx-props-no-spreading': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'] }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'] },
+    ],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
     'import/extensions': ['error', 'never', { svg: 'always' }],
